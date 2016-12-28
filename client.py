56fc24c09ccde4bd46c3ca7ede14224de9d2aec6
smuser90 <sam.musso90@gmail.com>
2016-12-28 08:27:13 -0700

    Succesfully parsing json

--- client.py
+++ client.py
@@ -1,4 +1,18 @@
 import json;
+import os;
+import signal;
+import sys;
+
+def clear_screen():
+    print(chr(27) + "[2J");
+    _unused = os.system("clear");
+
+def signal_handler(signal, frame):
+    print('\r\nShutting down... please wait.')
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
 JSONstr = '{"first_name": "Guido", "last_name":"Rossum"}'
 
 parsedJSON = json.loads(JSONstr);
@@ -18,7 +32,13 @@ def handle_message(message, *args):
         *args: Any additional arguments received, likely will be empty
     """
     parsedMessage = json.loads(message);
-    print(parsedMessage);
+    amount = parsedMessage['amount'];
+    cart = parsedMessage['cart'];
+    shopper = parsedMessage['shopper']
+
+    print("Amount: ",amount);
+    print("Cart: ",cart);
+    print("Shopper: ",shopper);
 
 with SocketIO(SOCKET_HOST) as sock:
     # Set up our message handler
